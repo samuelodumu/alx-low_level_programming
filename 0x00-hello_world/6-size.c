b8894811aaba49a10e3bc0039952660475aac2a1
samuel <themainsamuel@gmail.com>
2023-02-23 07:08:54 -0800

    Answer to task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
 /**
  * main - this function prints the sizes of different data types
  * Return: It returns a zero
+ *
  */
 
 int main(void)
@@ -12,10 +13,11 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %lu byte(s)", (unsigned long)(sizeof(c));
 	printf("Size of an int: %lu byte(s)", (unsigned long)(sizeof(i));
-	printf("Size of a long int: %lu byte(s)", (unsigned long)(sizeof(li);
-	printf("Size of a long long int: %lu byte(s)", (unsigned long)(sizeof(lli);
-	printf("Size of a float: %lu byte(s)", (unsigned long)(sizeof(f);	
+	printf("Size of a long int: %lu byte(s)", (unsigned long)(sizeof(li));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)(sizeof(lli));
+	printf("Size of a float: %lu byte(s)", (unsigned long)(sizeof(f));	
 	return (0);
 }
