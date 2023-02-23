277fb77a34e482be3c8badf029566e79a0e31600
samuel <themainsamuel@gmail.com>
2023-02-22 20:07:10 -0800

    Answer to task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - it prints a line to stdout using printf
+ * Return: it returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
