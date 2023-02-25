1cb7914fe454f7429cb514e6b7b1c3b6858ae2fc
samuel <themainsamuel@gmail.com>
2023-02-25 13:34:10 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int count;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (count = 26; count > -1 ; count--)
+	{
+		{
+			putchar(alpha[count]);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
