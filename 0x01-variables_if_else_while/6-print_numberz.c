2d70ab83b5a3667363f6062aebcf88b73c5f7b8c
samuel <themainsamuel@gmail.com>
2023-02-25 11:43:29 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers starting from zero
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(48 + num);
+	}
+	putchar('\n');
+	return (0);
+}
