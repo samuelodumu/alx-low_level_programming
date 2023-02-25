ab15356e52d08551eb1397652f63c226203aaa2b
samuel <themainsamuel@gmail.com>
2023-02-25 08:02:31 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, then uppercase.
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int count = 0;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	char ALPHA[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (count = 0; count < 27; count++)
+	{
+		putchar(alpha[count]);
+	}
+	for (count = 0; count <= 27; count++)
+	{
+		if (ALPHA[count] == 0)
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(ALPHA[count]);
+		}
+	}
+	return (0);
+}
