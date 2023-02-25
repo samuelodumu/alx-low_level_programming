636fb0b345108ce6eb5baac4e8316a3a9aa340b0
samuel <themainsamuel@gmail.com>
2023-02-25 02:53:24 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int count = 0;
+	char alpha[] = "abcdefghijklmnopqrstwxyz";
+
+	for (count = 0; count < 28; count++)
+	{
+		if (alpha[count] == 0)
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(alpha[count]);
+		}
+	}
+	return (0);
+}
