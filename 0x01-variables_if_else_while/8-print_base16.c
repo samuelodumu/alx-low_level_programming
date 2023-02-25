f29563d0e50e6b24902d1ac5e1e278d6318fd190
samuel <themainsamuel@gmail.com>
2023-02-25 14:13:03 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers starting from zero
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	int num;
+	char hex[] = "123456789abcdef";
+	
+	for (num = 0; num < 16; num++)
+	{
+		putchar(hex[num]);
+	}
+	putchar('\n');
+	return (0);
+}
