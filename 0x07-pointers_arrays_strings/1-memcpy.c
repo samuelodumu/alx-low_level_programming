26c3c46d0c3118c796bf7bfe2ea14d3f09396502
samuel <themainsamuel@gmail.com>
2023-03-06 18:27:49 -0800

    fix for task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies memory area
- * @dest: memory area
- * @src: source
- * @n: length of src
- *
- * Return: pointer to dest.
- */
+ * * _memcpy - copies memory area from @src to @dest
+ * * for @n chars
+ * * @dest: buffer to be copied into;
+ * * @src: buffer being copied from
+ * * @n: number of chars being copied
+ * *
+ * * Return: pointer to @dest
+ * */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-
 	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+		{
+			dest[i] = src[i];
+		}
+
+
+		return (dest);
 }
