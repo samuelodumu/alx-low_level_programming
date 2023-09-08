2bdd39992bcc60d7bdd8a6472b94872e3fe3a0ec
Samuel <themainsamuel@gmail.com>
2023-09-08 11:25:39 -0700

    task 6: Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
