17af5c3f110d1b86990ae24829ef10f505b8408b
Samuel <themainsamuel@gmail.com>
2023-09-22 03:52:20 -0700

    task 0: strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,10 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
