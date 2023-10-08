6d8cfc7f84543504b6705bbb5323f107b09208da
Samuel <themainsamuel@gmail.com>
2023-10-08 05:57:38 -0700

    modified task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,12 +13,13 @@ void puts2(char *str)
 		while (*str != '\0')
 		{
 			_putchar(*str);
-			str += 2;
+			str++;
 
 			if (*str == '\0')
 			{
 				break;
 			}
+			str++;
 		}
 		_putchar('\n');
 	}
