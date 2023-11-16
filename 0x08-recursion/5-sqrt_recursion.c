945b63cf4cc81388a7b1dd8b815d6c0745dacb11
Samuel Odumu <themainsamuel@gmail.com>
2023-11-16 17:23:56 +0100

    task 6: Inception. Is it possible?

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - prints the the natural square root of n
+ *
+ * @n: integer to print the square root of
+ *
+ * Return: the natural square root of n
+ */
+
+int _sqrt_recursion(int n);
+{
+	if (n  0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is prime
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int prime_number(int rem, int n);
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else if (prime_number(2, n) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * prime_number - helper function, determines if n is prime
+ *
+ * @i: a divider number
+ * @n: number given
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int prime_number(int i, int n)
+{
+	int rem = n % i;
+
+	if (i < n/2)
+	{
+		if (rem != 0)
+		{
+			i++;
+			return (prime_number(i, n));
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (rem);
+	}
+}
