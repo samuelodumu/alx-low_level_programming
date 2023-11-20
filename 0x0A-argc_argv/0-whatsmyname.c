0bcd2e5ac8c9ae703cfc2b34e7796911fa8b99fb
Samuel Odumu <themainsamuel@gmail.com>
2023-11-20 16:38:44 +0100

    task 0: It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_my_name - prints the first element of argv
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
