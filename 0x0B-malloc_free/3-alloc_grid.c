fb315575c1ff58ef00df311dad733e44b2b59333
Samuel Odumu <themainsamuel@gmail.com>
2023-11-23 22:19:29 +0100

    modified task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i <= height; i++)
 	{
-		ptr[i] = malloc(sizeof(int *) * (width));
+		ptr[i] = malloc(sizeof(int) * (width));
 
 		if (ptr[i] == NULL)
 		{
