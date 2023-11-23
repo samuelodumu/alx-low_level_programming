0d0efe64dede056b3d85969c218811adae2591fb
Samuel Odumu <themainsamuel@gmail.com>
2023-11-23 22:09:45 +0100

    task 3: If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: first integer
+ * @height: second integer
+ *
+ * Return: pointer to 2D array on success, NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= height; i++)
+	{
+		ptr[i] = malloc(sizeof(int *) * width);
+
+		if (ptr[i] == NULL)
+		{
+			while (i > -1)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
