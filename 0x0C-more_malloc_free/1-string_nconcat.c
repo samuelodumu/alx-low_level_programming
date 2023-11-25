bb5fe531c8c6fbec4b85f5c0e968ac5fcd3cdd75
Samuel Odumu <themainsamuel@gmail.com>
2023-11-25 08:00:14 +0100

    task 1: string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates the first n bytes of s2 to s1,
+ * and null terminates the new string
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from s2
+ *
+ * Return: pointer to newly allocated memory for concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	i = strlen(s1) + n + 1;
+	ptr = malloc(sizeof(char) * i);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		ptr[j] = s1[j];
+	}
+	for (k = 0; k < n; k++, j++)
+	{
+		if (s2[k] != '\0')
+		{
+			ptr[j] = s2[k];
+		}
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
