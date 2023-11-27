b6548e8f88943ddcd76123c5c245491384d3d14d
Samuel Odumu <themainsamuel@gmail.com>
2023-11-27 23:00:23 +0100

    modified task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_H_
+#ifndef _FUNCTION_LIKiE_MACRO_H_
 #define _FUNCTION_LILE_MACRO_H_
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x < 0) ? -(x) : (x))
 
 #endif /* _FUNCTION_LIKE_MACRO_H_ */
