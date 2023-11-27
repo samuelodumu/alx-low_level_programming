334e477381184f131e93cc159e8cf086d608f7ce
Samuel Odumu <themainsamuel@gmail.com>
2023-11-27 22:47:55 +0100

    task 3 answer is a ternary operator *weeps at how long it took me to figure out*

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LILE_MACRO_H_
 
-#define ABS(x)
-
-#if (ABS < 0)
-{
-	#undef ABS
-	#define ABS(x) ((-1) * (x))
-}
-
-#elif (ABS >= 0)
-{
-	#undef ABS
-	#define ABS(x) (x)
-}
-#endif /* ABS */
+#define ABS(x) ((x < 0) ? (-x) : (x))
 
 #endif /* _FUNCTION_LIKE_MACRO_H_ */
