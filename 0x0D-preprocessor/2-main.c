1d16b76f3a55688a34fdf0590790fde7e3f18b63
Samuel Odumu <themainsamuel@gmail.com>
2023-11-27 19:33:12 +0100

    task 2: File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints current file name
+ *
+ * Return: 0 on succces
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
