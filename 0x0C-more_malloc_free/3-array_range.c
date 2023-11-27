9878e3b0b24cdcd81f16352530ef27073aaea349
Samuel Odumu <themainsamuel@gmail.com>
2023-11-27 01:00:06 +0100

    task 3: array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,11 +15,24 @@
 
 int *array_range(int min, int max)
 {
-	unsigned int *ptr, i;
+	int *ptr, i, n;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	ptr = malloc(m
+	n = max - min + 1;
+	ptr = malloc(sizeof(int) * n);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[i] = min + i;
+	}
+	return (ptr);
+}
