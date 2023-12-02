13eb6b9dd7ed4ab5b64d97b5ec4c3b0a6804cffc
Samuel Odumu <themainsamuel@gmail.com>
2023-12-02 12:21:24 +0100

    task 1: If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTION_POINTERS_H */
