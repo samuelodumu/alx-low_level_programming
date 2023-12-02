008ffb9930f909a429f95bf6290152ad2dfc088a
Samuel Odumu <themainsamuel@gmail.com>
2023-12-02 15:27:56 +0100

    handled null values on task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include "function_pointers.iih"
 
 /**
  * int_index - searches for an integer
@@ -16,6 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
+		return (-1);
+	if (arrray == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
