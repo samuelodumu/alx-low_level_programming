b2b64d55b187d4ff1ec082d36b096a9eeaf3e774
Samuel Odumu <themainsamuel@gmail.com>
2023-12-12 18:47:12 +0100

    reverted change

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	if (h != NULL)
 	{
-		while (current != NULL || current != 0)
+		while (current != NULL)
 		{
 			printf("%d\n", current->n);
 			current = current->next;
