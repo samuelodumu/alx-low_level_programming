250895cb74d36805560738095a5a3ae4b4e996fe
Samuel Odumu <themainsamuel@gmail.com>
2023-12-12 15:53:14 +0100

    task 4: Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: first node in the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		free(current->str);
+		current = next_node;
+	}
+}
