472c3aaecd1cb30b41eeefeb70b35ba611d1b981
Samuel Odumu <themainsamuel@gmail.com>
2023-12-12 18:31:58 +0100

    task 0: Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current = h;
+	int i = 0;
+
+	if (h != NULL)
+	{
+		while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+			i++;
+		}
+		return (i);
+	}
+	return (0);
+}
