dcc3c3308df2867c776cd7743929162815070e1e
Samuel Odumu <themainsamuel@gmail.com>
2023-12-13 02:21:10 +0100

    task 5: Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the pointer to the first node of listint_t
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	if (current != NULL)
+	{
+		while (current->next != NULL)
+		{
+			free(current);
+			current = current->next;
+		}
+		free(current);
+	}
+	*head = NULL;
+}
