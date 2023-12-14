1490481c83a213d8847a3bc0c96c09975aa49eaf
Samuel Odumu <themainsamuel@gmail.com>
2023-12-14 04:54:31 +0100

    buggy task 10 code

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,8 +1,36 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - 
- *
+ * delete_nodeint_at_index - deletes the node at index index of
+ * a listint_t linked list
+ * @head: pointer to the pointer to the first node
+ * @index: the index of the node that should be deleted
+ * Return: 1 if it succeeded, -1 if it failed
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)i
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp = *head, *prev;
+
+	if (head == NULL || temp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; temp != NULL && i < index - 1; i++)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	if (temp == NULL)
+	{
+		return (-1);
+	}
+	prev->next = temp->next;
+
+	free(temp);
+	free(prev);
+
+	return (1);
+}
