7f3c5f2b5fc0ec45a7237a47d1e64dee2f09bde1
Samuel Odumu <themainsamuel@gmail.com>
2023-12-14 03:18:04 +0100

    removed some code redundancy

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,11 +30,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
-	for (; i <= idx; i++)
-	{
-		if (!current)
-			return (NULL);
-	}
+	if (!current)
+		return (NULL);
 
 	if (newnode)
 	{
