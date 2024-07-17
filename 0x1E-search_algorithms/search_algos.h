83476410dbd86a2ea11cdf39a9f0c8c3c7debaed
Samuel <themainsamuel@gmail.com>
2024-07-17 15:13:13 +0100

    without stdlib.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
 int binary_search(int *array, size_t size, int value);
